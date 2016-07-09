c92b2bbafa576a2cbbd1dca0dbec4daf5d5acafe
Bruno S Reis <bruno.reis@useboom.net>
2016-07-09 16:34:33 -0300

    sec5/lec49 Summary Pipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,26 @@
 import {Component} from 'angular2/core';
-import {TweetComponent} from "./tweet.component";
-import {TweetService} from "./TweetService";
+import {SummaryPipe} from "./summary.pipe";
 
 @Component({
   selector: 'my-app',
-  directives: [TweetComponent],
-  providers: [TweetService],
+  pipes: [SummaryPipe],
   template: `
-            <div *ngFor="#tweet of tweets">
-              <tweet [tweet]="tweet"></tweet>
-            </div>
+            {{post.title}}
+            <br />
+            {{post.body | summary:20}}
             `
 })
 
 export class AppComponent {
-  tweets;
-
-  constructor(tweetService: TweetService) {
-    this.tweets = tweetService.getTweets();
+  post = {
+    title: 'Angular tutorial',
+    body: `
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Quisque et tortor quis quam tempor fringilla. Sed sodales nulla vehicula feugiat posuere. 
+          Nullam tempor imperdiet rutrum. Sed ac varius dui, scelerisque interdum quam.
+          In ut metus sed elit faucibus fermentum. In hac habitasse platea dictumst.
+          Nunc at facilisis mauris, non pharetra augue.
+          `
   }
   
 }
