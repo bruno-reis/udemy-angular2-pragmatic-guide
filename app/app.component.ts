72b8abf3d747115dac3d6949c2bb667d66c9b716
bruno-reis <bruno.reis@useboom.net>
2016-07-15 09:43:12 -0300

    sec10/lec115 Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,21 @@
-import {Component, OnInit} from 'angular2/core';
-import {PostService} from './section9/post.service';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {AlbumsComponent} from "./section10/albums.component";
+import {AlbumComponent} from "./section10/album.component";
+import {ContactComponent} from "./section10/contact.component";
+
+@RouteConfig([
+  {path: '/albums', name: 'Albums', component: AlbumsComponent, useAsDefault: true},
+  {path: '/albums/:id', name: 'Album', component: AlbumComponent},
+  {path: '/contact', name: 'Contact', component: ContactComponent},
+  {path: '/*other', name: 'Other', redirectTo: ['Albums']}
+])
 
 @Component({
-  selector: 'my-app',
-  providers: [PostService, HTTP_PROVIDERS],
-  template: `
-             <div *ngIf="isLoading">
-              <i class="fa fa-spinner fa-spin fa-3x"></i>
-             </div>
-            `
+    selector: 'my-app',
+    templateUrl: '/app/app.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
-
-export class AppComponent implements OnInit{
-  isLoading = true;
-
-  constructor(private _postService: PostService) {
-  }
-
-  ngOnInit() {
-    this._postService.getPosts()
-      .subscribe( posts => {
-        this.isLoading = false;
-        console.log(posts)
-      });
-  }
+export class AppComponent {
 }
